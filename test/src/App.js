c3bc1764128c4f2ac41e23fedd3217f856e6d5b6
KJAVA19811815 <karunsingh1993@gmail.com>
2017-10-23 17:46:01 +0000

    can render data in the component

--- test/src/App.js
+++ test/src/App.js
@@ -3,6 +3,22 @@ import "./App.css";
 import Pokemon from "./components/Pokemon";
 
 class App extends Component {
+  renderPokemon() {
+    if (this.state.pokemon === null) {
+      return <p>NULL</p>;
+    } else {
+      return this.state.pokemon.map(function(poke) {
+        return <p key={poke.id}>{poke.name}</p>;
+      });
+    }
+  }
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      pokemon: []
+    };
+  }
   componentDidMount() {
     return fetch("http://localhost:8080/", {
       method: "get",
@@ -14,13 +30,15 @@ class App extends Component {
         return response.json();
       })
       .then(json => {
-        console.log(json);
+        this.setState({
+          pokemon: json
+        });
       });
   }
   render() {
     return (
       <div>
-        <Pokemon />
+        <Pokemon pokemon={this.state.pokemon} />
       </div>
     );
   }
--- test/src/components/Pokemon.js
+++ test/src/components/Pokemon.js
@@ -1,44 +1,28 @@
 import React, { Component } from "react";
 
 export default class Pokemon extends Component {
-  // renderPokemon() {
-  //   if (this.state.pokemon === null) {
-  //     return <p>NULL</p>;
-  //   } else {
-  //     return this.state.pokemon.map(function(poke) {
-  //       return <p key={poke.id}>{poke.name}</p>;
-  //     });
-  //   }
-  // }
-  // constructor(props) {
-  //   super(props);
-  //
-  //   this.state = {
-  //     pokemon: null
-  //   };
-  // }
-
-  // componentDidMount() {
-  //   return fetch("http://localhost:8080/", {
-  //     method: "get",
-  //     headers: {
-  //       Accept: "application/json"
-  //     }
-  //   })
-  //     .then(function(response) {
-  //       return response.json();
-  //     })
-  //     .then(json => {
-  //       this.setState({
-  //         pokemon: [json]
-  //       });
-  //     });
-  // }
-
+  renderPokemon = () => {
+    if (this.props.pokemon.length === 0) {
+      return <p>NO POKEMON</p>;
+    } else {
+      return this.props.pokemon.map(poke => {
+        return (
+          <div>
+            NAME:
+            <h2 key={poke.id}>{poke.name}</h2>
+            <br />
+            TYPE:
+            <p>{poke.type}</p>;
+          </div>
+        );
+      });
+    }
+  };
   render() {
     return (
       <div>
         <h1>TEST</h1>
+        {this.renderPokemon()}
       </div>
     );
   }
