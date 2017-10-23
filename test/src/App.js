d91ffd751c29676d058a275c10c0195a8d8c67ba
KJAVA19811815 <karunsingh1993@gmail.com>
2017-10-23 15:52:25 +0000

    code refactor and sending data to props

--- test/src/App.js
+++ test/src/App.js
@@ -3,6 +3,20 @@ import "./App.css";
 import Pokemon from "./components/Pokemon";
 
 class App extends Component {
+  componentDidMount() {
+    return fetch("http://localhost:8080/", {
+      method: "get",
+      headers: {
+        Accept: "application/json"
+      }
+    })
+      .then(function(response) {
+        return response.json();
+      })
+      .then(json => {
+        console.log(json);
+      });
+  }
   render() {
     return (
       <div>
